a02b782536b9e3a916f9306ec4f61011215d2631
Denys Kyslytsia <kislica779@gmail.com>
2023-09-19 18:46:50 +0300

    Changed ShoeInfo

--- src/pages/shoe-info/ShoeInfo.tsx
+++ src/pages/shoe-info/ShoeInfo.tsx
@@ -6,20 +6,13 @@ import { showMessage } from "../../components/toast/show-message";
 import { addShoeInCart } from "../../store/account-sclice/accountSlice";
 
 import "./ShoeInfo.css";
+import { ShoeInfoProps } from "./types";
+import Size from "../../components/card/size/Size";
 
-type ShoeInfoProps = {
-  id?: number;
-};
-
-const ShoeInfo: React.FC<ShoeInfoProps> = ({ id }) => {
+const ShoeInfo = ({ id }: ShoeInfoProps) => {
   const shoes = useAppSelector((state) => state.shoes.value);
   const [shoeSize, setShoeSize] = useState<number | null>(null);
-  const [colorSize40, setColorSize40] = useState<boolean>(false);
-  const [colorSize41, setColorSize41] = useState<boolean>(false);
-  const [colorSize42, setColorSize42] = useState<boolean>(false);
-  const [colorSize43, setColorSize43] = useState<boolean>(false);
-  const [colorSize44, setColorSize44] = useState<boolean>(false);
-  const [colorSize45, setColorSize45] = useState<boolean>(false);
+  const [coloredSize, setColoredSize] = useState<number | null>(null);
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
@@ -76,111 +69,11 @@ const ShoeInfo: React.FC<ShoeInfoProps> = ({ id }) => {
                   </div>
 
                   <div className="size-block">
-                    <div className="size">
-                      <h3>Size : </h3>
-                      <span
-                        style={{
-                          backgroundColor: colorSize40 ? " #2b2f4c" : "",
-                          color: colorSize40 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(40);
-                          setColorSize40(true);
-                          setColorSize41(false);
-                          setColorSize42(false);
-                          setColorSize43(false);
-                          setColorSize44(false);
-                          setColorSize45(false);
-                        }}
-                      >
-                        40
-                      </span>
-                      <span
-                        style={{
-                          backgroundColor: colorSize41 ? " #2b2f4c" : "",
-                          color: colorSize41 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(41);
-                          setColorSize40(false);
-                          setColorSize41(true);
-                          setColorSize42(false);
-                          setColorSize43(false);
-                          setColorSize44(false);
-                          setColorSize45(false);
-                        }}
-                      >
-                        41
-                      </span>
-                      <span
-                        style={{
-                          backgroundColor: colorSize42 ? " #2b2f4c" : "",
-                          color: colorSize42 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(42);
-                          setColorSize40(false);
-                          setColorSize41(false);
-                          setColorSize42(true);
-                          setColorSize43(false);
-                          setColorSize44(false);
-                          setColorSize45(false);
-                        }}
-                      >
-                        42
-                      </span>
-                      <span
-                        style={{
-                          backgroundColor: colorSize43 ? " #2b2f4c" : "",
-                          color: colorSize43 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(43);
-                          setColorSize40(false);
-                          setColorSize41(false);
-                          setColorSize42(false);
-                          setColorSize43(true);
-                          setColorSize44(false);
-                          setColorSize45(false);
-                        }}
-                      >
-                        43
-                      </span>
-                      <span
-                        style={{
-                          backgroundColor: colorSize44 ? " #2b2f4c" : "",
-                          color: colorSize44 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(44);
-                          setColorSize40(false);
-                          setColorSize41(false);
-                          setColorSize42(false);
-                          setColorSize43(false);
-                          setColorSize44(true);
-                          setColorSize45(false);
-                        }}
-                      >
-                        44
-                      </span>
-                      <span
-                        style={{
-                          backgroundColor: colorSize45 ? " #2b2f4c" : "",
-                          color: colorSize45 ? "white" : "",
-                        }}
-                        onClick={() => {
-                          setShoeSize(45);
-                          setColorSize40(false);
-                          setColorSize41(false);
-                          setColorSize42(false);
-                          setColorSize43(false);
-                          setColorSize44(false);
-                          setColorSize45(true);
-                        }}
-                      >
-                        45
-                      </span>
-                    </div>
+                    <Size
+                      coloredSize={coloredSize}
+                      setShoeSize={setShoeSize}
+                      setColoredSize={setColoredSize}
+                    />
                   </div>
 
                   <div className="buy-block block">
