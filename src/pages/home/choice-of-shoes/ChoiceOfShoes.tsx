181bdd7118d27402e85ae9c7f1be971702e6c32d
Denys Kyslytsia <kislica779@gmail.com>
2024-10-06 17:26:32 +0300

    Changed size photo for cobblers

--- src/pages/home/choice-of-shoes/ChoiceOfShoes.tsx
+++ src/pages/home/choice-of-shoes/ChoiceOfShoes.tsx
@@ -4,10 +4,12 @@ const ChoiceOfShoes: React.FC = () => {
   return (
     <div className="choice-conteiner">
       <div className="choice-img"></div>
+
       <div className="choice-info-block">
         <h2 className="choice-info-block__title">
           What men's shoes to choose?
         </h2>
+
         <div className="choice-info-block__text">
           Good men's classic shoes are one of the priority items in a man's
           wardrobe list. Even if you rarely wear a formal suit, you should still
@@ -15,6 +17,7 @@ const ChoiceOfShoes: React.FC = () => {
           needs to know what types of shoes are and how you can choose the right
           pair for yourself.
         </div>
+
         <div className="choice-info-block__button">
           <a
             href="https://brammels.com/style/suit-and-shoes/"
